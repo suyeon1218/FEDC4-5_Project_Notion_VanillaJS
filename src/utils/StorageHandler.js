a3409fe10c56bab03576e19cc3ca6fa8dec12ebf
Suyeon <msjgr2801@gmail.com>
2023-06-28 18:23:28 +0900

    Feat: 토글 정보를  로컬 스토리지 저장하는 기능 구현

---
+++ src/utils/StorageHandler.js
@@ -0,0 +1,25 @@
+export const setOpenDocument = (id) => {
+  try {
+    localStorage.setItem(id, true);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export const setCloseDocumet = (id) => {
+  try {
+    localStorage.removeItem(id);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export const getCurrOpenState = (id) => {
+  try {
+    const openState = localStorage.getItem(id);
+    
+    return openState === null ? false : true;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
